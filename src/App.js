649f6456d63d07756511c35e0fda6817fcba2a21
iinaamasum <iinaamasum@gmail.com>
2022-07-26 16:15:07 +0600

    hero section added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Home from './Pages/HomePage/Home/Home';
 import NavBar from './Shared/NavBar/NavBar';
 
 function App() {
   return (
     <>
       <NavBar />
+      <Home />
     </>
   );
 }
---
+++ src/Pages/HomePage/HeroSection/HeroSection.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import heroImg from '../../../assets/icons/PicsArt_04-14-04.42 1.svg';
+import './HeroSection.css';
+
+const HeroSection = () => {
+  return (
+    <section className="hero__section">
+      <div className="d-flex justify-content-between align-items-center container mx-auto hero__heading">
+        {/* header text  */}
+        <div className="hero__text">
+          <h1>
+            <span>Accelerate Innovation</span> <br />
+            <span>with Global AI Challenges</span>
+          </h1>
+          <p>
+            AI Challenges at DPhi simulate real-world problems. It is a great
+            place to put your AI/Data Science skills to test on diverse datasets
+            allowing you to foster learning through competitions.{' '}
+          </p>
+          <Button className="hero__btn">Create Challenge</Button>
+        </div>
+        {/* image section  */}
+        <div className="hero__img">
+          <img className="" src={heroImg} alt="" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
