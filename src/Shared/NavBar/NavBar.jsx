03f28dfdd482f95110e008053421f32d8ca0ce7d
iinaamasum <iinaamasum@gmail.com>
2022-07-29 01:16:20 +0600

    navbar links array added

--- src/Shared/NavBar/NavBar.jsx
+++ src/Shared/NavBar/NavBar.jsx
@@ -6,6 +6,11 @@ import logo from '../../assets/icons/logo.svg';
 import './NavBar.css';
 
 const NavBar = () => {
+  const navLinks = [
+    { id: 1, path: '/', name: 'Home' },
+    { id: 2, path: '/create-challenge', name: 'Create Challenge' },
+    { id: 3, path: '/#challenges', name: 'Challenges' },
+  ];
   return (
     <section>
       <Navbar className="fixed-top nav__bar bg-light" expand="lg">
@@ -16,9 +21,11 @@ const NavBar = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto nav__link">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#challenges">Challenges</Nav.Link>
-              <Nav.Link href="/create-challenge">Add Challenges</Nav.Link>
+              {navLinks.map((link) => (
+                <Nav.Link href={link.path} key={link.id}>
+                  {link.name}
+                </Nav.Link>
+              ))}
             </Nav>
           </Navbar.Collapse>
         </Container>
